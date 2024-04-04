7277f8ba4c0d4fc98230d0d71337f59b40492eb9
Angelina Cortes <cortesporcz@gmail.com>
2024-04-04 16:26:37 -0500

    also changing server.js file - fixing errors and typos

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
-const express = require("express");
-const db = require("./config/connection");
-const routes = require("./routes");
+const express = require('express');
+const connection = require('./config/connection');
+const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(routes);
 
-db.once("open", () => {
+app.use('/api', routes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
   app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
+    console.log(`Server is running on http://localhost:${PORT}`);
   });
 });
